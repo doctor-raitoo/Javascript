9b74ff3d2d05d272dc2839e7f485cb36c6722393
rhaditya1508 <rhadityaprastito@gmail.com>
2026-01-02 00:03:06 +0700

    Second commit: Data Structure

--- Data Structure/object.js
+++ Data Structure/object.js
@@ -1,8 +1,26 @@
 const mahasiswa = {
-    nama: 'Rhaditya',
+    namaDepan: 'Rhaditya',
+    namaBelakang: 'Prastito',
     nim: 2405181049,
     jurusan: 'Teknik Komputer dan Informatika',
     prodi: 'Teknologi Rekayasa Perangkat Lunak'
 };
 
-console.log(mahasiswa.nama);
+// mengakses properti menggunakan dot notation
+console.log(mahasiswa.namaDepan);
+
+// mengakses properti menggunakan square bracket
+console.log(mahasiswa['jurusan']);
+
+// mengakses menggunakan destructuring object
+const {namaDepan, namaBelakang} = mahasiswa;
+console.log(namaDepan, namaBelakang);
+
+// mengubah nilai properti
+mahasiswa.prodi = 'Teknik Elektro';
+console.log(mahasiswa.prodi);
+
+// menghapus properti
+delete mahasiswa.prodi;
+console.log(mahasiswa);
+

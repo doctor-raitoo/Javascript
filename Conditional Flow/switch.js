152323384227835526d9c9b3876006e7f472e06d
rhaditya1508 <rhadityaprastito@gmail.com>
2026-01-15 22:30:53 +0700

    Conditional Flow: Switch Case

---
+++ Conditional Flow/switch.js
@@ -0,0 +1,13 @@
+const buah = 'Jagung';
+
+switch (buah) {
+    case 'Pisang':
+        console.log('Pisang');
+        break;
+
+    case 'Apel':
+        console.log('Apel');
+        break;
+    default:
+        console.log('Bukan buah');
+}

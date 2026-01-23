ebca8793d059f8e5a674b1777de9540c7aca26ab
rhaditya1508 <rhadityaprastito@gmail.com>
2026-01-23 21:37:32 +0700

    Practical test of Flow Control

--- Flow Control/practical_test.js
+++ Flow Control/practical_test.js
@@ -1,19 +1,26 @@
-function categorizeNumber (input) {
-    for (let i = 0; input.length; i++) {
-        if (typeof input[i] !== 'number') {
-            throw new Error ('Input tidak valid')
-        }
+function categorizeNumber(input) {
+if (typeof input !== 'number' || !Number.isInteger(input)) {
+    throw new Error('Input harus berupa bilangan bulat');
+}
+
+if (input === 0) return 'Nol';
+if (input < 0) return 'Negatif';
+
+const isPrime = (n) => {
+    if (n <= 1) return false;
+    for (let i = 2; i * i <= n; i++) {
+        if (n % i === 0) return false;
     }
+    return true;
+};
+
+if (isPrime(input)) return 'Prima';
+
+return input % 2 === 0 ? 'Genap' : 'Ganjil';
 }
 
 console.log(categorizeNumber(15)); 
 console.log(categorizeNumber(12)); 
-console.log(categorizeNumber(17));
+console.log(categorizeNumber(17)); 
 console.log(categorizeNumber(0)); 
 console.log(categorizeNumber(-5));
-
-try {
-    categorizeNumber('abc');
-} catch (error) {
-    console.log(error.message);
-}
